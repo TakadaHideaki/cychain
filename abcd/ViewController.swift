69a935f591c93a488c4fa1afba61f2c1777d8c58
takadahideaki007 <takadahideaki@takadahideakinoMacBook-Pro.local>
2018-12-31 01:11:43 +0900

    Initial Commit

---
+++ abcd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  abcd
+//
+//  Created by takadahideaki007 on 2018/12/31.
+//  Copyright © 2018 高田英明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
