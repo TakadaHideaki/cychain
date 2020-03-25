2e9b3d63bb2fe6ec25e31d89002e094ed39a78a9
takada hideaki <takadahideaki007@gmail.com>
2020-03-25 21:45:04 +0900

    storegetURL訂正

--- cychain/AppDelegate.swift
+++ cychain/AppDelegate.swift
@@ -15,8 +15,7 @@ import XCGLogger
 let UD = UserDefaults.standard
 let USER_ID = Auth.auth().currentUser?.uid
 //#elseif cychain-dev
-//let STORAGE = Storage.storage().reference(forURL: "gs://cychain-dev.appspot.com")
-let STORAGE = Storage.storage().reference(forURL: "gs://cychain-6d3b6.appspot.com")
+let STORAGE = Storage.storage().reference(forURL: "gs://cychain-dev.appspot.com")
 
 
 
